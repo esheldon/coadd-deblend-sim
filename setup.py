0059f7417579f03383978420df8f6f958d15138f
Erin Sheldon <erin.sheldon@gmail.com>
2017-06-05 14:35:10 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import distutils
+from distutils.core import setup, Extension, Command
+import os
+import numpy
+
+
+setup(
+    name="coaddsim", 
+    packages=['coaddsim'],
+    version="0.1",
+)
+
+
+
+
