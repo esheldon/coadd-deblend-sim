f8b0a9d400b73ba027f0deda9fe0c37df7052c32
Erin Sheldon <erin.sheldon@gmail.com>
2017-06-07 16:13:39 -0400

    restructure and fix bugs
    restructured so that obslist is taken  on construction
    
    fix bugs; the biggest one was setting the image directly
    
    obs.image = image
    which should be done with the ethod
    obs.set_image(image)
    
    since the image was the wrong data type this was causing
    problems for the fitters

--- coaddsim/coadd.py
+++ coaddsim/coadd.py
@@ -8,46 +8,110 @@ import nsim
 import ngmix
 import numpy as np
 import galsim
+import copy
 
 class CoaddImages():
 
-    def __init__(self, interp='lanczos3', size=48, center=24.5):
+    def __init__(self, observations, interp='lanczos3', target_psf=None):
+        self.observations = observations
+        self.target_psf=target_psf
         self.interp = interp
+
+        self._add_obs()
+
+    def get_mean_coadd(self, find_cen=False):
+        """
+        perform a weight mean coaddition
+
+        parameters
+        ----------
+        find_cen: bool, optional
+            If True, set the jacobian center based on a fit
+        """
+
+        weights = 1./self.vars
+        weights /= weights.sum()
+
+        coadd_obs = self.coadd_obs
+        wcs = coadd_obs.jacobian.get_galsim_wcs()
+        psf_wcs = coadd_obs.psf.jacobian.get_galsim_wcs()
+
+        coadd = galsim.Sum([image*w for image,w in zip(self.images,weights)])
+
+        ny,nx = coadd_obs.image.shape
+        psf_ny,psf_nx = coadd_obs.psf.image.shape
+
+        coadd_image = galsim.Image(nx, ny, wcs=wcs)
+        coadd.drawImage(image=coadd_image)
+
+        coadd_psf = galsim.Sum([psf*w for psf,w in zip(self.psfs,weights)])
+        coadd_psf_image = galsim.Image(psf_nx, psf_ny, wcs=psf_wcs)
+        coadd_psf.drawImage(image=coadd_psf_image)
+
+        coadd_obs.set_image(coadd_image.array)
+        coadd_obs.psf.set_image(coadd_psf_image.array)
+
+        weight_map = np.zeros(self.coadd_obs.weight.shape)
+        coadd_var = (self.vars*weights*weights).sum()
+        weight_map[:,:] = 1./coadd_var
+        self.coadd_obs.set_weight(weight_map)
+
+
+        crow, ccol = self.canonical_center
+        if find_cen:
+            moments = galsim.hsm.FindAdaptiveMom(galsim.Image(self.coadd_obs.image))
+            centroid = moments.moments_centroid
+
+            shift = galsim.PositionD(ccol, crow) - centroid
+            self.coadd_obs.jacobian.set_cen(col=shift.x, row=shift.y)
+        else:
+            # center the jacobian on the canonical center
+            self.coadd_obs.jacobian.set_cen(row=crow, col=ccol)
+
+        return self.coadd_obs
+
+
+    def _add_obs(self):
+
         self.images = []
         self.psfs = []
-        self.vars = []
-        self.fitters = []
+        self.vars = np.zeros(len(self.observations))
+
         self.coadd_obs = None
-        self.size = size
-        self.center = center
 
-
-    def add_obs(self, observations, target_psf=None):
-
-        for obs in observations:
+        for i,obs in enumerate(self.observations):
 
             if self.select_obs(obs) is False:
                 continue
 
-            # currently using scale of the first image for the coadd scale
+            # currently using image size and scale of the first image for the coadd scale
 
             if self.coadd_obs is None:
-                self.coadd_obs = obs
-                self.scale = obs.jacobian.get_scale()
+                self.coadd_obs = copy.deepcopy(obs)
+                self.canonical_center = (np.array(self.coadd_obs.image.shape)-1.0)/2.0
 
             # interplated image, shifted to center of the postage stamp
-            #wcs = galsim.JacobianWCS(obs.jacobian.dudcol, obs.jacobian.dudrow, obs.jacobian.dvdcol, obs.jacobian.dvdrow)
             wcs = obs.jacobian.get_galsim_wcs()
 
-            image = galsim.InterpolatedImage(galsim.Image(obs.image,wcs=wcs), x_interpolant=self.interp)
-            psf = galsim.InterpolatedImage(galsim.Image(obs.psf.image,wcs=wcs), x_interpolant=self.interp)
-            sky_shift = -wcs.toWorld(galsim.PositionD(*obs.meta['offset_pixels']))
+            image = galsim.InterpolatedImage(
+                galsim.Image(obs.image,wcs=wcs),
+                x_interpolant=self.interp,
+            )
+            psf = galsim.InterpolatedImage(
+                galsim.Image(obs.psf.image,wcs=wcs),
+                x_interpolant=self.interp,
+            )
+
+            yoffset, xoffset = obs.meta['offset_pixels']
+            sky_shift = -wcs.toWorld(galsim.PositionD(x=xoffset, y=yoffset))
 
             image = image.shift(sky_shift)
 
-            if target_psf is not None:
+            if self.target_psf is not None:
+                raise NotImplementedError("need to normalize psf correctly so weight maps are consistent")
+
                 psf_inv = galsim.Deconvolve(psf)
-                matched_image = galsim.Convolve([image, psf_inv, target_psf])
+                matched_image = galsim.Convolve([image, psf_inv, self.target_psf])
                 self.images.append(matched_image)
             else:
                 self.images.append(image)
@@ -57,44 +121,12 @@ class CoaddImages():
             self.psfs.append(psf)
 
             # assume variance is constant
-            var = 1./(np.mean(obs.weight))
-            self.vars.append(var)
+            var = 1./obs.weight.max()
 
-            # fit parameters
-            fitter = ngmix.admom.run_admom(obs, 4*self.scale**2)
-            self.fitters.append(fitter)
-        self.vars = np.array(self.vars)
+            self.vars[i] = var
+
 
     def select_obs(self, observation):
         return True
 
-    def get_mean_coadd(self):
-
-        weights = 1./self.vars
-        weights /= weights.sum()
-
-        coadd = galsim.Sum([image*w for image,w in zip(self.images,weights)])
-        coadd_image = galsim.Image(self.size, self.size, scale=self.scale)
-        coadd.drawImage(image=coadd_image)
-
-        coadd_psf = galsim.Sum([psf*w for psf,w in zip(self.psfs,weights)])
-        coadd_psf_image = galsim.Image(self.size, self.size, scale=self.scale)
-        coadd_psf.drawImage(image=coadd_psf_image)
-
-        self.coadd_obs.image = coadd_image.array
-        self.coadd_obs.psf.image = coadd_psf_image.array
-
-        weight_map = np.zeros(self.coadd_obs.weight.shape)
-        coadd_var = (self.vars*weights*weights).sum()
-        weight_map[:,:] = 1./coadd_var
-        self.coadd_obs.set_weight(weight_map)
-
-        moments = galsim.hsm.FindAdaptiveMom(galsim.Image(self.coadd_obs.image))
-        centroid = moments.moments_centroid
-        shift = galsim.PositionD(self.center, self.center) - centroid
-
-        self.coadd_obs.jacobian.set_cen(col=shift.x, row=shift.y)
-        fitter = ngmix.admom.run_admom(self.coadd_obs, 4*self.scale**2)
-
-        return self.coadd_obs
 
--- coaddsim/test.py
+++ coaddsim/test.py
@@ -1,3 +1,10 @@
+from __future__ import print_function
+try:
+    raw_input
+except:
+    raw_input=input
+    xrange=range
+
 from .coadd import CoaddImages
 
 try:
@@ -5,64 +12,118 @@ try:
 except ImportError:
     pass
 
-EXAMPLE_CONF = {
-    'images': {
-        'noise': 0.35,
-        'object': {
-            'cen_shift': {
-                'type': 'uniform',
-                'radius': 20,
-            },
-            'nepoch': 100,
-            'stamp_size': [48, 48],
-        },
-        'psf': {
-            's2n': 10000,
-            'stamp_size': [48, 48],
-        },
-        'wcs': {
-            'dudx': 0.0,
-            'dudy': 0.263,
-            'dvdx': 0.263,
-            'dvdy': 0.0,
-        }
-    },
-    'object': {
-        'model': 'bdk',
-        'fracdev': {
-            'type': 'uniform',
-            'range': [0.0, 1.0],
-        },
-        'g': {
-            'type': 'ba',
-            'sigma': 0.2,
-        },
-        'r50_flux': {
-            'type': 'cosmos',
-        },
-    },
-    'psf': {
-        'beta': 3.5,
-        'model': 'moffat',
-        'r50': 0.54,
-        'shape': [0.0, 0.0],
-    },
-    'shear': [0.02, 0.0],
-}
+_confstring="""
+# multi-epoch
+# joint r50-flux from cosmos
+# designed so min s/n is about 10
+# 6 epochs like recent HSC release
 
-def test(seed=2342, nepoch=100):
-    coadder = CoaddImages()
+images:
 
-    conf={}
-    conf.update(EXAMPLE_CONF)
+    noise: 0.10
+
+    psf:
+        s2n: 10000
+        stamp_size: [48,48] # optional
+
+    object:
+        nepoch: 10
+
+        stamp_size: [48,48]
+
+        cen_shift:
+            type: "uniform"
+            radius: 1 # pixels
+            #radius: 0 # pixels
+
+    wcs:
+      dudx: 0.0
+      dudy: 0.263
+      dvdx: 0.263
+      dvdy: 0.0
+
+psf:
+    model: "moffat"
+
+    beta: 3.5
+    # same as fwhm = 0.9
+    r50: 0.54
+        #type: "uniform"
+        #range: [0.41,0.6]
+
+    shape: [0.0, 0.0]
+
+
+object:
+    model: "bdk"
+
+    g:
+        type: "ba"
+        sigma: 0.2
+
+    r50_flux:
+        type: "cosmos"
+
+
+    # fraction of flux in the bulge
+    fracdev:
+        type: "uniform"
+        range: [0.0, 1.0]
+
+shear: [0.02, 0.00]
+"""
+
+def getconf():
+    import yaml
+    return yaml.load(_confstring)
+
+def test(seed=2342,
+         num=1,
+         nepoch=10,
+         doplot=False,
+         run_admom=False):
+
+    conf=getconf()
     conf['images']['object']['nepoch'] = nepoch
 
     sim = nsim.sime.Sim(conf, seed) 
 
-    obslist = sim()
+    for i in xrange(num):
+        obslist = sim()
+
+        coadder = CoaddImages(obslist)
+        coadd_obs = coadder.get_mean_coadd()
+        coadd_obs = coadder.get_mean_coadd()
 
 
-    coadder.add_obs(obslist)
-    coadd_obs = coadder.get_mean_coadd()
+        if run_admom:
+            import ngmix
+            jac = coadd_obs.jacobian
+            Tguess=4.0*jac.get_scale()**2
+            #Tguess=0.5
 
-    return coadd_obs
+            for i in xrange(4):
+                fitter=ngmix.admom.run_admom(coadd_obs, Tguess)
+                res=fitter.get_result()
+                if res['flags'] == 0:
+                    print("    admom s2n:",res['s2n'])
+                    print("    admom pars:",res['pars'])
+
+                    if doplot:
+                        import images
+                        mim=fitter.get_gmix().make_image(
+                            coadd_obs.image.shape,
+                            jacobian=jac,
+                        )
+                        images.multiview(mim, title='model')
+                    break
+                else:
+                    print("    admom failed",res['flags'],res['flagstr'])
+
+        if doplot:
+            import images
+            images.multiview(coadd_obs.image, title='coadd')
+            if num > 1:
+                if raw_input('hit a key (q to quit): ')=='q':
+                    return
+
